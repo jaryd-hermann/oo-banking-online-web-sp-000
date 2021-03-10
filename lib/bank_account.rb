f1068b1a5ba0ca4a1270bd6b3ee3f0877411d9d5
Jaryd Hermann <hermannjaryd@gmail.com>
2021-03-10 12:15:22 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -2,9 +2,15 @@ class BankAccount
 attr_accessor :name, :balance, :status
 
 def initialize(name)
-  @name = name.freeze
+  @name = name
   @balance = 1000
   @status = "open"
 end
+
+def name
+  @name.freeze
+end
+
+
 
 end
