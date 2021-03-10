fa6bf93b482f8b652fa95c141da1b177adf3a87c
Jaryd Hermann <hermannjaryd@gmail.com>
2021-03-10 12:42:23 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,5 +1,5 @@
 class Transfer
-attr_accessor :sender, :reciever, :amount
+attr_accessor :sender, :receiver, :amount
 
 def initialize(sender, receiver, amount)
   @sender = sender #BankAccount class instance
