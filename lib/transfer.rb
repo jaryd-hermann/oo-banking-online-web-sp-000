178fcb2f1add0afe9d5cf23a11b89af2228d18d4
Jaryd Hermann <hermannjaryd@gmail.com>
2021-03-10 12:38:19 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -8,5 +8,12 @@ def initialize(sender, receiver, amount)
   @status = "pending"
 end
 
+def valid?(sender, receiver)
+  if sender.valid? && receiver.valid?
+    true
+  else
+    false
+  end
+end
 
 end
