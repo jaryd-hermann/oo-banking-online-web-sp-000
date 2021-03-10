27a5fc94cb4e244b0f1b0013ddfbcd1e6d69683b
Jaryd Hermann <hermannjaryd@gmail.com>
2021-03-10 12:35:26 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,7 +1,7 @@
 class Transfer
 attr_accessor :sender, :reciever, :amount
 
-def initialize(sender, reciever, amount)
+def initialize(amount)
   @sender = sender
   @receiver = reciever
   @amount = amount
